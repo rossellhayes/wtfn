85fda020396a1bb182c5a5d85869086a2d46ed27
Alex Rossell Hayes <alexander@rossellhayes.com>
2022-10-03 20:36:57 -0700

    refactor(function_under_cursor): overhaul function
    - Adjust `code` for tabs rather than `cursor`
    - Generate character return value by extracting text from `code` instead of `parse_data`

--- R/wtfn_addin.R
+++ R/wtfn_addin.R
@@ -14,43 +14,69 @@ function_under_cursor <- function() {
 
 	context <- rstudioapi::getActiveDocumentContext()
 	cursor <- context$selection[[1]]$range$start
-	cursor <- adjust_for_tabs(cursor, context)
 
-	parse <- parse(text = context$contents, keep.source = TRUE)
-	parse_data <- utils::getParseData(parse)
+	# `getActiveDocumentContext()` and `getParseData()` handle tabs differently.
+	# `getActiveDocumentContext()` treats a tab as one character,
+	# but `getParseData()` treats a tab as eight characters.
+	# That means if `context$contents` contains any tabs,
+	# the position of the cursor in `getActiveDocumentContext()` won't match
+	# the position of the expressions in `getParseData()`.
+	# To fix this, we convert each tab to a single space,
+	# which both functions count as one character.
+	code <- gsub("\t", " ", context$contents, fixed = TRUE)
+
+	parse_data <- parse(text = code, keep.source = TRUE)
+	parse_data <- utils::getParseData(parse_data)
 
 	found_symbol <- parse_data[
 		parse_data$line1 == cursor["row"] &
 			parse_data$col1 <= cursor["column"] &
 			parse_data$col2 >= cursor["column"] &
+			# An "expr" token is a container that holds other tokens.
+			# We want the most specific token (probably a "SYMBOL_FUNCTION_CALL"),
+			# not any exprs that contain that token.
 			parse_data$token != "expr",
 	]
 
+	# Found symbol may not contain the entire expression under the cursor.
+	# For example, a namespaced function contains three symbols:
+	# `namespace`, `::` and `function`.
+	# It wouldn't be helpful if we returned "::"
+	# when the cursor was over the colons in a namespaced function.
+	# Instead, we return all symbols that share `found_symbol`'s parent expression.
 	found_symbol_parent <- found_symbol[["parent"]]
 	found_expr <- parse_data[parse_data$parent == found_symbol_parent, ]
-	code <- paste(found_expr[["text"]], collapse = "")
-	code
-}
 
-adjust_for_tabs <- function(cursor, context) {
-	# `rstudioapi::getActiveDocumentContext()` treats a tab as one character,
-	# but `utils::getParseData()` treats a tab as eight characters.
-	# To find the symbol under the cursor, we must adjust the cursor position
-	# to account for the different way tabs are counted.
+	# Using the positions of the found expressions in the parse data,
+	# extract the function under the cursor from `code`.
+	found_start <- found_expr[found_expr$line1 == min(found_expr$line1), ]
+	found_start <- found_start[found_start$col1 == min(found_start$col1), ]
 
-	text_on_cursor_line <- context$contents[cursor["row"]]
-	text_before_cursor <- substr(text_on_cursor_line, 1, cursor["column"] - 1)
+	found_end <- found_expr[found_expr$line2 == max(found_expr$line2), ]
+	found_end <- found_end[found_end$col2 == max(found_end$col2), ]
 
-	tabs_before_cursor <- unlist(gregexpr("\t", text_before_cursor, fixed = TRUE))
-	# If there are no matches, `gregexpr()` returns -1, which we want to exclude
-	tabs_before_cursor <- tabs_before_cursor[tabs_before_cursor > 0]
-	tabs_before_cursor <- length(tabs_before_cursor)
+	code <- line_sub(
+		code,
+		line_start = found_start$line1,
+		col_start  = found_start$col1,
+		line_end   = found_end$line2,
+		col_end    = found_end$col2
+	)
 
-	if (tabs_before_cursor == 0) {
-		return(cursor)
+	if (length(code) == 1) {
+		return(code)
 	}
 
-	cursor["column"] <- cursor["column"] + 7 * tabs_before_cursor
-	cursor
+	paste(code, collapse = "\n")
 }
 
+line_sub <- function(x, line_start, col_start, line_end, col_end) {
+	if (line_start == line_end) {
+		return(substr(x[[line_start]], col_start, col_end))
+	}
+
+	x <- x[seq.int(line_start, line_end)]
+	x[[1]] <- substr(x, col_start, .Machine$integer.max)
+	x[[length(x)]] <- substr(x, 0, col_end)
+	x
+}
