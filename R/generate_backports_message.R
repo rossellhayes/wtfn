1e49a24d84043664a6442d5bbee778c85c56c96b
Alex Rossell Hayes <alexander@rossellhayes.com>
2022-11-12 14:41:12 -0800

    fix(generate_backports_message): format message inline

--- R/generate_backports_message.R
+++ R/generate_backports_message.R
@@ -1,20 +1,24 @@
 generate_backports_message <- function(fun, description) {
 	minimum_r_version <- get_package_minimum_r_version(description)
 
-	is_backported <- any(
+	backport <- backports[
 		backports$fun == fun$bare_name &
 			backports$package == fun$pkg &
-			backports$version > minimum_r_version
-	)
+			backports$version > minimum_r_version,
+	]
 
-	if (is_backported) {
+	if (nrow(backport) > 0) {
+		backport_version <- backport$version[[1]]
+
+		cli::cli_div(theme = cli_theme_wtfn())
+
 		c(
-			"!" = paste(
-				"The current implementation of {fun$cli_name}",
-				"was introduced in R {.val {backport$version}}."
+			"!" = cli::format_inline(
+				"The current implementation of {fun$cli_name} ",
+				"was introduced in R {.val {backport_version}}."
 			),
-			"*" = paste(
-				"For compatibility with R < {.val {backport$version}},",
+			"*" = cli::format_inline(
+				"For compatibility with R < {.val {backport_version}}, ",
 				"consider using {.help [{.pkg backports}](backports::import)}."
 			)
 		)
