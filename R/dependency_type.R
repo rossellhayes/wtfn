41bb39d6efb03d9440d8d256a2dcf43a26764089
Alex Rossell Hayes <alexander@rossellhayes.com>
2022-11-27 14:24:25 -0800

    fix(dependency_type): add default argument and ensure `description` is normalized

--- R/dependency_type.R
+++ R/dependency_type.R
@@ -1,4 +1,4 @@
-dependency_type <- function(pkg, description) {
-	deps <- description$get_deps()
+dependency_type <- function(pkg, description = desc::description$new()) {
+	deps <- description$normalize()$get_deps()
 	deps[deps$package == pkg, "type"][1]
 }
