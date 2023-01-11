83dec79ce8b6a1e66d8d1684950f86743292d81d
Garrick Aden-Buie <garrick@adenbuie.com>
2023-01-11 12:29:33 -0500

    Ask user for the function when the console is active (#14)
    * feat: If addin is run with console active, ask the user for the function
    
    * refactor(wtfn_addin): implement question mark bullet using `cli` theme
    
    * feat(wtfn_addin): ask user to input function whenever no symbol is found under the cursor (not just when in the console)
    
    Co-authored-by: Alex Rossell Hayes <alexander@rossellhayes.com>

--- R/wtfn.R
+++ R/wtfn.R
@@ -6,7 +6,7 @@ wtfn <- function(fun) {
 	backports_message <- generate_backports_message(fun, description)
 	wtfn_status <- get_wtfn_status(fun, description, namespace_imports)
 
-	cli::cli_div(theme = cli_theme_wtfn())
+	cli::cli_div(theme = cli_theme_wtfn)
 	cli::cli_inform(wtfn_status$message)
 	if (length(backports_message) > 0) cli::cli_inform(backports_message)
 	invisible(wtfn_status$can_use)
@@ -16,19 +16,25 @@ unquote <- function(x) {
 	gsub("[\"']", "", x, perl = TRUE)
 }
 
-cli_theme_wtfn <- function() {
-	list(
-		span.run = list(transform = function(x) {
+cli_theme_wtfn <- list(
+	span.run = list(
+		transform = function(x) {
 			x <- cli::builtin_theme()$span.run$transform(x)
 			cli::builtin_theme()$span.code$transform(x)
-		}),
-		span.var = list(
-			color = "blue",
-			transform = function(x) {
-				if (cli::ansi_grepl("^`.*`$", x)) return(x)
-				if (cli::ansi_grepl("`", x, fixed = TRUE)) return(paste0("`` ", x, " ``"))
-				paste0("`", x, "`")
-			}
-		)
+		}
+	),
+	span.var = list(
+		color = "blue",
+		transform = function(x) {
+			if (cli::ansi_grepl("^`.*`$", x)) return(x)
+			if (cli::ansi_grepl("`", x, fixed = TRUE)) return(paste0("`` ", x, " ``"))
+			paste0("`", x, "`")
+		}
+	),
+	`.bullets .bullet-?` = list(
+		`text-exdent` = 2,
+		before = function(x) {
+			paste0(cli::col_cyan(cli::style_bold("?")), " ")
+		}
 	)
-}
+)
